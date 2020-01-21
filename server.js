5426583ef5cf6b0a9dabab7ad0ae390a85fd048f
Leonardo Matheus <44291248+leonardomath@users.noreply.github.com>
2020-01-20 23:36:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-j5sgp.mongodb.net/nodeapi?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://username:<password>@cluster0-j5sgp.mongodb.net/nodeapi?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
